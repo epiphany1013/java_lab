71c0528e956716190ca99804376dad3e03be3d9a
dev <hisoftly@naver.com>
2024-05-13 21:17:25 +0900

    Add Chapter 02 code

---
+++ GodOfJava/src/Chapter02/HelloBasicJava.java
@@ -0,0 +1,12 @@
+package Chapter02;
+
+public class HelloBasicJava {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello God of Java !");
+        System.out.println("I love this book.");
+        System.out.print("Hello God of Java !");
+        System.out.print("I love this book.");
+    }
+}
